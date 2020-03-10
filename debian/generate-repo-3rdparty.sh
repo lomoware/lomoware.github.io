19bc147378033acf7eaddf8e00fa80ab4dbae8db
Jeromy Fu <fuji246@gmail.com>
2020-03-10 16:19:52 -0700

    update lomo-web

--- debian/generate-repo-3rdparty.sh
+++ debian/generate-repo-3rdparty.sh
@@ -1,11 +1,11 @@
 cd jessie
-#reprepro remove jessie lomo-vips
+reprepro remove jessie lomo-vips
 reprepro -P 1  -S main includedeb jessie ../../lomo-vips.deb
-#reprepro remove jessie lomo-omxplayer
+reprepro remove jessie lomo-omxplayer
 reprepro -P 1  -S main includedeb jessie ../../lomo-omxplayer_jessie.deb
 
 cd ../buster
-#reprepro remove buster lomo-vips
+reprepro remove buster lomo-vips
 reprepro -P 1  -S main includedeb buster ../../lomo-vips.deb
-#reprepro remove buster lomo-omxplayer
+reprepro remove buster lomo-omxplayer
 reprepro -P 1  -S main includedeb buster ../../lomo-omxplayer_buster.deb
--- debian/generate-repo-lomoweb.sh
+++ debian/generate-repo-lomoweb.sh
@@ -1,5 +1,7 @@
 cd jessie
+reprepro remove jessie lomo-web
 reprepro -P 1  -S main includedeb jessie ../../lomo-web.deb
 
 cd ../buster
+reprepro remove buster lomo-web
 reprepro -P 1  -S main includedeb buster ../../lomo-web.deb
