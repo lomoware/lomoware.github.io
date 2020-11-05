5889b9f544a7f3e19b995ed615e0080e6491c07b
Jeromy Fu <fuji246@gmail.com>
2020-11-05 14:38:50 -0800

    update lomod
    Signed-off-by: leslie-wang <leslie.qiwa@gmail.com>

--- debian/generate-repo-lomobase.sh
+++ debian/generate-repo-lomobase.sh
@@ -5,3 +5,7 @@ reprepro -P 1  -S main includedeb jessie ../../lomo-base.deb
 cd ../buster
 reprepro remove buster lomo-base
 reprepro -P 1  -S main includedeb buster ../../lomo-base.deb
+
+cd ../focal
+reprepro remove focal lomo-base
+reprepro -P 1  -S main includedeb focal ../../lomo-base.deb
---
+++ debian/generate-repo_x86.sh
@@ -0,0 +1,3 @@
+cd focal
+reprepro remove focal lomo-backend
+reprepro -P 1  -S main includedeb focal ../../lomo-backend_amd64.deb
