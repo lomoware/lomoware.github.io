bd232a3e30359322c33edac948813a2a6171d564
Jeromy Fu <fuji246@gmail.com>
2022-01-30 20:30:48 -0800

    update vips

--- debian/generate-repo-3rdparty.sh
+++ debian/generate-repo-3rdparty.sh
@@ -11,3 +11,11 @@ cd ../bullseye
 reprepro remove bullseye lomo-vips
 reprepro -P 1  -S main includedeb bullseye ../../lomo-vips_armhf.deb
 reprepro -P 1  -S main includedeb bullseye ../../lomo-vips_arm64.deb
+
+cd ../focal
+reprepro remove focal lomo-vips
+reprepro -P 1  -S main includedeb focal ../../lomo-vips_amd64.deb
+
+cd ../groovy
+reprepro remove groovy lomo-vips
+reprepro -P 1  -S main includedeb groovy ../../lomo-vips_amd64.deb
--- debian/generate-repo-3rdparty_x86.sh
+++
@@ -1,3 +0,0 @@
-cd focal
-reprepro remove focal lomo-vips
-reprepro -P 1  -S main includedeb focal ../../lomo-vips_amd64.deb
